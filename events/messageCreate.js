b2ea7216493e24725f871fc00ca6c70563da28e5
pizuzupka-alt <pizuzupka@post.cz>
2025-08-11 19:30:07 +0200

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,1 +1,10 @@
+// events/messageCreate.js
+module.exports = {
+    name: 'messageCreate',
+    once: false,
+    execute(message, client) {
+        // nech prázdné, nebo jen logování
+        // console.log(`Nová zpráva od ${message.author.tag}: ${message.content}`);
+    }
+};
 
